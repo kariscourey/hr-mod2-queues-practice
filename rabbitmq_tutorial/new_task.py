de8d3f1203094e5b47c8c7a7fa66b0ff68754ee9
karis courey <kariscourey@gmail.com>
2022-10-04 00:56:07 -0400

    add rmq tut

---
+++ rabbitmq_tutorial/new_task.py
@@ -0,0 +1,16 @@
+import sys, pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+# channel.queue_declare(queue='hello')
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message,
+                      properties=pika.BasicProperties(
+                         delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
+                      ))
+print(" [x] Sent %r" % message)
